c1e2db65f50345619a96ba1760b9367049eb411b
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-05-16 10:27:58 -0400

    primer commit

---
+++ src/main/java/cl/app/ms_product_bs/model/dto/ProductDTO.java
@@ -0,0 +1,15 @@
+package cl.app.ms_product_bs.model.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class ProductDTO {
+    private long id;
+    private String name;
+
+}
---
+++ src/test/java/cl/app/ms_product_bs/MsProductBsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.app.ms_product_bs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
